194119384a6b586b909837410fcce7b95dd42df9
Qingqing Yang <qingqingyang@hotmail.co.uk>
2024-01-14 11:18:46 +0000

    add variables for selection

---
+++ assets/js/logic.js
@@ -0,0 +1,25 @@
+var timerEl = document.querySelector("#time");
+
+// Start screen
+var startButton = document.querySelector("#start");
+var startScreenEl = document.querySelector("#start-screen");
+// Questions screen
+var questionsScreenEl = document.querySelector("#questions");
+var choicesEl = document.querySelector("#choices");
+var feedbackEl = document.querySelector("#feedback");
+// End screen
+var endScreenEl = document.querySelector("#end-screen");
+var initialsInput = document.querySelector("#initials");
+var finalScoreEl = document.querySelector("#final-score");
+var submitButton = document.querySelector("#submit");
+
+
+console.log(startButton);
+
+var timerOnDisplay = 75;
+var questionIndex = 0;
+
+// A start button that when clicked a timer starts and the first question appears.
+startButton.addEventListener("click", function () {
+
+});
