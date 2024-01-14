aa057a0d16c1bf16d506da994a45bb98959090d6
Qingqing Yang <qingqingyang@hotmail.co.uk>
2024-01-14 08:55:41 +0000

    add questions in the js file

---
+++ assets/js/questions.js
@@ -0,0 +1,34 @@
+var question1 = "Commonly used data types DO NOT include:";
+var question1Answers = ["1.strings",
+    "2.booleans",
+    "3.alerts",
+    "4.numbers"];
+var answerOfQuestion1 = "3.alerts";
+
+var question2 = "The condition in an if/else statement is enclosed within ____.";
+var question2Answers = ["1.quotes",
+    "2.curly brackets",
+    "3.parentheses",
+    "4.square brackets"];
+var answerOfQuestion2 = "2.curly brackets";
+
+var question3 = "Arrays in JavaScript can be used to store ____.";
+var question3Answers = ["1.numbers and strings",
+    "2.other arrays",
+    "3.booleans",
+    "4.all of the above"];
+var answerOfQuestion3 = "4.all of the above";
+
+var question4 = "String values must be enclosed within ____ when being assigned to variable.";
+var question4Answers = ["1.commas",
+    "2.curly brackets",
+    "3.quotes",
+    "4.parentheses"];
+var answerOfQuestion4 = "3.quotes";
+
+var question5 = "A very useful tool used during development and debugging for printing content to the debugger is :";
+var question5Answers = ["1.JavaScript",
+    "2.terminal/bash",
+    "3.for loops",
+    "4.console.log"];
+var answerOfQuestion5 = "4.console.log";
